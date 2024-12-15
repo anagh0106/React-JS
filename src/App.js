35a07bda79df76661a494b22789c24d55665b7d5
Anagh Patel <anagh0106@gmail.com>
2024-12-15 11:06:19 +0530

    Communication

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Footer } from './components/Footer';
+// import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 // import { Home } from './components/Home';
 import { Content } from './components/Content';
+import { University } from './components/University';
 
 function App() {
 
-  var title = "React JS"
-  var city = "Gandhinagar"
+  const users = ["Aman", "Anagh", "Achyut", "Ram"]
 
-  const address = {
-    city: "Gandhinagar",
-    state: "Gujarat",
-    country: "India"
-  }
-
+  const students = [
+    {
+      id: 1,
+      name: "Aman",
+      age: 20
+    },
+    {
+      id: 2,
+      name: "Anagh",
+      age: 21
+    },
+    {
+      id: 3,
+      name: "Achyut",
+      age: 22
+    },
+  ]
   return (
+
     <div className="App">
-      <Header title={title} city={city}></Header>
-      {/* <Home></Home> */}
-      <Content address={address}></Content>
-      <Footer></Footer>
+      <Header></Header>
+
+      {/* Normal Printed */}
+      {
+        <h1>{users}</h1>
+      }
+
+      {/* Iterate Using Map Method 
+          => Map method return 
+          => forEach Can't return
+      */}
+      {
+        users.map((user) => {
+          return <h1 style={{ color: user.length > 4 ? "red" : "green" }}>{user}</h1>
+        })
+      }
+
+      {/* Want to do it with json Object */}
+      {
+        students.map((stu) => {
+          return <h1>{stu.name}</h1>
+        })
+      }
+      <University />
     </div>
   );
 }
